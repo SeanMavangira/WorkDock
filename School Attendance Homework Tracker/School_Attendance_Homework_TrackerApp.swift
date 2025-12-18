27fca0a305028d9f380cf94a5358734fa8e89fd3
Sean <mavangirasean@gmail.com>
2025-12-18 10:42:10 +0200

    Initial Commit

---
+++ School Attendance Homework Tracker/School_Attendance_Homework_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  School_Attendance_Homework_TrackerApp.swift
+//  School Attendance Homework Tracker
+//
+//  Created by Sean Mavangira on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct School_Attendance_Homework_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
